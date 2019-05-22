16729a474063662bb82fe9371421b32ae6d5314d
Ewan Lockwood <ewanlockwood4@icloud.com>
2019-05-22 10:55:15 +0000

    Connected to Heroku and MongoDB

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'the_book_stop'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@alphacluster-lqak4.mongodb.net/the_book_stop?retryWrites=true'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return '<h1> Hello </h1>'
-    
+@app.route('/libary')
+def get_books():
+    return render_template("libary.html", books=mongo.db.books.find())
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
