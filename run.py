b6662767d062fc497ee26c5bf3b1ed50283bea06
Ewan Lockwood <ewanlockwood4@icloud.com>
2019-05-31 10:26:36 +0000

    Finished landing page and began on the library page

--- run.py
+++ run.py
@@ -1,22 +1,46 @@
+# IMPORT START
+
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
+# IMPORT END
+
+# APP START
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'the_book_stop'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@alphacluster-lqak4.mongodb.net/the_book_stop?retryWrites=true'
 
 mongo = PyMongo(app)
 
+# APP ROUTING START 
 
 @app.route('/')
-@app.route('/libary')
-def get_books():
-    return render_template("libary.html", books=mongo.db.books.find())
+@app.route('/index')
+def index():
+    return render_template("index.html", genres=mongo.db.genres.find())
 
+    
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    users = mongo.db.users
+    users.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+    
+@app.route('/library')
+def library():
+    return render_template("library.html", books=mongo.db.books.find())
+    
+# APP ROUTING END
+
+#APP ENVIRONMENT START
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
+#APP ENVIRONMENT END
+            
+# APP END
